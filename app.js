181474b0ab482367d9bd70a2045da57315245fc7
Agustín Firpo <agustin_firpoutlook.com.ar>
2023-11-23 15:51:35 -0300

    CONFIGURE THE DIV CONTAINER FOR THE RANGE BAR

--- app.js
+++ app.js
@@ -3,14 +3,7 @@ const storageUsed = document.getElementById("storage-used-number");
 const storageLeft = document.getElementById("number");
 
 inputRange.addEventListener("input", () => {
-  usedValue = parseInt(inputRange.value);
+  const usedValue = parseInt(inputRange.value);
   storageUsed.innerText = usedValue;
   storageLeft.innerText = 1000 - usedValue;
-  updateProgressBar;
 });
-
-function updateProgressBar() {
-  const value =
-    ((inputRange.value - inputRange.min) / (inputRange.max - inputRange.min)) * 100;
-  inputRange.style.background = `linear-gradient(to right, #4CAF50 0%, #4CAF50 ${value}%, #ddd ${value}%, #ddd 100%)`;
-}
